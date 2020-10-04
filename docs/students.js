2cb41f58e3c29b2a7384cc6e3f1457f1977ca76b
Shubham Jagdhane <shubhamjagdhane1010@gmail.com>
2020-10-04 08:13:08 +0530

    webpage for student-information is added

---
+++ docs/students.js
@@ -0,0 +1,10 @@
+function selected_batch() {
+  d = document.getElementById("batch-name").value;
+  alert(d)
+}
+
+
+function selected_course() {
+  d = document.getElementById("course-name").value;
+  alert(d)
+}
